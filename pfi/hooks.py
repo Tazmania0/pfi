ff94cd9ba141c3f7ce9a8b2f23b81a9cc12e2f6b
tazmania0 <tzmail2000@gmail.com>
2025-03-20 14:15:58 +0200

    fix

--- pfi/hooks.py
+++ pfi/hooks.py
@@ -37,8 +37,8 @@ doc_events = {
 
 doc_events = {
     "Work Order": {
-        "before_save": "pfi.custom_scripts.work_order_customization.before_save",
-        "on_submit": "pfi.custom_scripts.work_order_customization.on_submit"
+        "before_save": "pfi.pfi.custom_scripts.work_order_customization.before_save",
+        "on_submit": "pfi.pfi.custom_scripts.work_order_customization.on_submit"
     }
 }
 
@@ -50,13 +50,13 @@ doc_events = {
 
 # hooks.py
 override_doctype_class = {
-    "BOM": "pfi.custom_scripts.bom_service_check.CustomBOM"
+    "BOM": "pfi.pfi.custom_scripts.bom_service_check.CustomBOM"
 }
 
-include_js = {
+include_js = [
     "public/js/pfi.js",
     "public/js/JsBarcode.all.min.js" 
-}
+]
 
 fixtures = [
 #    {"dt": "Custom Field", "filters": [["module", "=", "PFI"]]},
