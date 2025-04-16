f1b7422850f12ad3821831369462d2e0d86f72bd
tazmania0 <tzmail2000@gmail.com>
2025-04-16 15:57:21 +0300

    START

--- pfi/hooks.py
+++ pfi/hooks.py
@@ -17,9 +17,9 @@ app_license = "MIT"
 # required_apps = []
 
 # Include JS only if file exists
-app_include_js = [
-    "assets/pfi/js/work_order.js"
-]
+#app_include_js = [
+#    "assets/pfi/js/work_order.js"
+#]
 
 doc_events = {
 #    "Work Order": {
@@ -50,22 +50,22 @@ doc_events = {
 
 # hooks.py
 override_doctype_class = {
-    "BOM": "pfi.custom_scripts.bom_service_check.CustomBOM",
+#    "BOM": "pfi.custom_scripts.bom_service_check.CustomBOM",
 #    "Work Order": "pfi.custom_scripts.work_order_custom.CustomWorkOrder"
 }
 
 include_js = [
-    "public/js/pfi.js",
-    "public/js/JsBarcode.all.min.js" 
+#    "public/js/pfi.js",
+#    "public/js/JsBarcode.all.min.js" 
 ]
 
 fixtures = [
 #    {"dt": "Custom Field", "filters": [["module", "=", "PFI"]]},
 #    {"dt": "Property Setter", "filters": [["module", "=", "PFI"]]},
-    "Custom Field",
+#    "Custom Field",
 #    "Cutting Matrix Table",
 #    "Planned Quantity Table",
-    "Print Format/Job Card A8"
+#    "Print Format/Job Card A8"
 ]
 
 
@@ -75,7 +75,7 @@ doctype_js = {
 #    "Work Order": "public/js/work_order_custom.js",
 #    "Cutting Matrix Table": "doctype/manufacturing/cutting_matrix_table/cutting_matrix_table.js",
 #    "Size Table": "doctype/manufacturing/size_table/size_table.js",
-    "BOM": "public/js/bom.js"
+#    "BOM": "public/js/bom.js"
 
 #    "Job Card": "public/js/job_card_custom.js"
 
@@ -83,7 +83,7 @@ doctype_js = {
 
 after_migrate = [
     # Existing BOM patch
-    "pfi.patches.v1_0_0.add_custom_field_to_bom.execute"
+    #"pfi.patches.v1_0_0.add_custom_field_to_bom.execute"
     # New Work Order override activation
     #"pfi.work_order_custom.activate_work_order_overrides"
     #"pfi.patches.v1_0_0.set_naming_defaults.execute"
@@ -92,29 +92,3 @@ after_migrate = [
 
 
 # hooks.py
-from frappe import _
-
-def get_data():
-    return {
-        # App metadata
-        "app_name": "pfi",
-        "app_title": "PFI",
-        "app_description": "Custom ERPNext App for Garment Manufacturing and Job Card Customization",
-        
-#        # Define your module(s)
-#        "modules": [
-#            {
-#                "label": _("PFI"),  # Display name
-#                "items": ["Cutting Matrix Table", "Planned Quantity Table" ]  # List DocTypes/pages in this module
-#            }
-#        ],
-        
-        # Other hooks (fixtures, doctype_js, etc.)
-        "doctype_js": {"BOM": "public/js/bom.js"},
-        "fixtures": ["Custom Field", "Print Format"]
-    }
-
-#def after_migrate():
-#    from frappe import reload_doctype
-#    reload_doctype("Work Order")
-#    frappe.publish_realtime('msgprint', 'Work Order overrides reloaded')
