28635e2550757db24bf59538b48bc2346df85472
tazmania0 <tzmail2000@gmail.com>
2025-04-22 15:34:50 +0300

    1

--- pfi/service.py
+++ pfi/service.py
@@ -42,15 +42,18 @@ def generate_qr_code(item_code):
     # return """<img src="/files/barcode/{}.png" alt="barcode" style="width:100%;height:100%;">""".format(filename)
 
 @frappe.whitelist(allow_guest=True)
+@frappe.whitelist()
 def generate_barcode_svg(item_code):
     from barcode import Code128
     from barcode.writer import SVGWriter
     from io import BytesIO
 
     options = {
-        'module_width': 0.4,
+        'module_width': 0.35,
         'quiet_zone': 10,
-        'write_text': False
+        'write_text': True,  # 🔥 This is the key to include the text below the barcode
+        'font_size': 10,
+        'text_distance': 1  # distance between barcode and text
     }
 
     buffer = BytesIO()
@@ -58,7 +61,7 @@ def generate_barcode_svg(item_code):
     svg_data = buffer.getvalue().decode("utf-8")
     buffer.close()
 
-    # Strip <?xml version="1.0" ... ?> header if present
+    # Strip <?xml version="1.0"?> if present
     if svg_data.startswith("<?xml"):
         svg_data = svg_data.split("?>", 1)[1].strip()
 
