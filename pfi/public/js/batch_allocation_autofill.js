59bb88d65b6756fa091cf706ae5313a879caf3ef
tazmania0 <tzmail2000@gmail.com>
2025-04-24 13:01:55 +0300

    1

--- pfi/public/js/batch_allocation_autofill.js
+++ pfi/public/js/batch_allocation_autofill.js
@@ -34,3 +34,28 @@ function auto_fill_remaining_qty(frm, cdt, cdn) {
         frappe.msgprint(__('All quantity has already been allocated to batches.'));
     }
 }
+
+
+frappe.ui.form.on('Work Order', {
+    onload: function(frm) {
+        // Attach handler to batch_allocations grid after form loads
+        frm.fields_dict["batch_allocations"].grid.on("add_row", function(grid_row) {
+            let row = grid_row.doc;
+
+            // Calculate total allocated quantity from existing rows
+            let total_allocated = 0;
+            (frm.doc.batch_allocations || []).forEach(r => {
+                if (r.name !== row.name) {
+                    total_allocated += r.batch_qty || 0;
+                }
+            });
+
+            let remaining = frm.doc.qty - total_allocated;
+            console.log("New row added. Remaining qty:", remaining);
+
+            if (remaining > 0) {
+                frappe.model.set_value(row.doctype, row.name, "batch_qty", remaining);
+            }
+        });
+    }
+});
