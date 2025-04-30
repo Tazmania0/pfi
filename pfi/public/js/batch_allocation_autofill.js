2812d35519a67cb47017d49e3a2a1d6b978835f5
tazmania0 <tzmail2000@gmail.com>
2025-04-30 15:40:14 +0300

    1

--- pfi/public/js/batch_allocation_autofill.js
+++ pfi/public/js/batch_allocation_autofill.js
@@ -48,3 +48,37 @@ function prefill_remaining_qty(frm, cdt, cdn) {
         auto_fill_remaining_qty(frm, cdt, cdn);
     }
 }
+
+
+frappe.ui.form.on('Work Order', {
+    refresh: function(frm) {
+        update_batch_allocation_summary(frm);
+    },
+    batch_allocations_on_form_rendered: function(frm) {
+        update_batch_allocation_summary(frm);
+    },
+    batch_allocations_add: function(frm) {
+        update_batch_allocation_summary(frm);
+    },
+    batch_allocations_remove: function(frm) {
+        update_batch_allocation_summary(frm);
+    }
+});
+
+
+function update_batch_allocation_summary(frm) {
+    let total_allocated = 0;
+    (frm.doc.batch_allocations || []).forEach(row => {
+        total_allocated += flt(row.batch_qty);
+    });
+
+    let overproduction_percentage = flt(frappe.defaults.get_default("overproduction_percentage_for_work_order")) || 0;
+    let allowed_qty = flt(frm.doc.qty) * (1 + overproduction_percentage / 100);
+    let remaining_qty = allowed_qty - total_allocated;
+
+    let summary_html = `<div style="padding: 8px; background-color: #f7f7f7; border: 1px solid #d1d8dd; border-radius: 4px;">
+        <strong>Total Allocated:</strong> ${total_allocated} / ${allowed_qty} (Remaining: ${remaining_qty})
+    </div>`;
+
+    frm.fields_dict.batch_allocation_summary.$wrapper.html(summary_html);
+}
