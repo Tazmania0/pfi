6b6a71bb7fd7590e472756c715b687c851d2fdca
tazmania0 <tzmail2000@gmail.com>
2025-05-09 15:08:11 +0300

    1

--- pfi/scripts/job_cards.py
+++ pfi/scripts/job_cards.py
@@ -162,6 +162,7 @@ class WorkOrder(ERPNextWorkOrder):
     def prepare_data_for_job_card_batchwise(self, row, index, plan_days, enable_capacity_planning):
         from copy import deepcopy
         from frappe.utils import date_diff
+        from frappe import get_link_to_form  # <-- Missing import added here
         from erpnext.manufacturing.doctype.work_order.work_order import create_job_card as create_job_card_standalone
         #from collections import defaultdict
         
